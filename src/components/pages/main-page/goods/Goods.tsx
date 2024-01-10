4fbb253d090ce87efc47ba50ce657870ef830102
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-10 22:51:22 +0200

    Worked with Goods components

--- src/components/pages/main-page/goods/Goods.tsx
+++ src/components/pages/main-page/goods/Goods.tsx
@@ -1,28 +1,41 @@
 'use client'
 import React, {useEffect, useState} from 'react';
-import styles from './Goods.module.scss'
-import axios, {Axios} from "axios";
-import Loading from "@/components/common/ui/loading/Loading";
-import GoodsCard from "@/components/pages/main-page/goods/subcomponents/goods-card/GoodsCard";
+import {fetchData} from '@/components/pages/api/dataService';
+import {GoodsCard} from "@/components/pages/main-page/goods/subcomponents/goods-card/GoodsCard";
 
-
-
-function Goods() {
-    const[goods, setGoods] = useState(null)
+const Goods = () => {
+    const [products, setProducts] = useState<any[]>([]);
 
     useEffect(() => {
-        axios.get('https://dummyjson.com/products')
-            .then(res => {
-                setGoods(res.data)
-            })
+        const fetchProducts = async () => {
+            try {
+                const data = await fetchData();
+                setProducts(data.products);
+            } catch (error) {
+                console.error('Error fetch data:', error);
+            }
+        };
+
+        fetchProducts();
     }, []);
 
     return (
-        <section className={styles.goods}>
-            {goods ? <GoodsCard/> : <Loading/>}
-        </section>
+        <>
+            {products.map(item => (
+                <GoodsCard
+                    key={item.id}
+                    id={item.id}
+                    title={item.title}
+                    description={item.description}
+                    price={item.price}
+                    discountPercentage={item.discountPercentage}
+                    rating={item.rating}
+                    brand={item.brand}
+                    category={item.category}
+                    images={item.images}/>
+            ))}
+        </>
     );
-}
+};
 
 export default Goods;
-
--- src/components/pages/main-page/goods/subcomponents/goods-card/GoodsCard.tsx
+++ src/components/pages/main-page/goods/subcomponents/goods-card/GoodsCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from "next/link";
 import styles from './GoodsCard.module.scss'
+import Image from "next/image";
+
 
 type Props = {
     "id": number,
@@ -9,23 +11,32 @@ type Props = {
     "price": number,
     "discountPercentage": number,
     "rating": number,
-    "stock": number,
     "brand": string,
     "category": string,
-    "thumbnail": string,
-    "images":[string]
+    "images": string[]
 }
 
-function GoodsCard({ id, title, description, price, discountPercentage, rating, stock, brand, category, thumbnail, images }:Props ) {
+export const GoodsCard = ({ id, title, description, price, discountPercentage, rating, brand, category, images }:Props ) => {
+
     return (
         <>
             <article className={styles.card} key={id}>
                 <Link href={`/${id}`}>
-
+                    <div className={styles.cardImg}>
+                        <Image
+                            src={images[2]}
+                            width={400}
+                            height={300}
+                            alt={description}/>
+                    </div>
+                    <div>{title}</div>
+                    <div>Brand: {brand}</div>
+                    <div>Price: {price}</div>
+                    <div>Discount: {discountPercentage}%</div>
+                    <div>Rating: {rating}</div>
+                    <div>Category: {category}</div>
                 </Link>
             </article>
         </>
     );
 }
-
-export default GoodsCard;
