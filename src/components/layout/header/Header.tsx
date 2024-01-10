21752c968171023eaf4a3836204df2cfe91774be
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-10 13:30:27 +0200

    Created Header layout component

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar, Toolbar} from "@mui/material";
+
+function Header() {
+    return (
+        <AppBar>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
