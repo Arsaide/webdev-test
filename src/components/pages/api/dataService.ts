55667cae8d4abf43e73669fafef7964753fc8d34
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-10 22:50:52 +0200

    API Axios

---
+++ src/components/pages/api/dataService.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    brand: string;
+    category: string;
+    images: string[];
+}
+
+interface ApiResponse {
+    products: Product[];
+}
+
+const API_URL = 'https://dummyjson.com/products';
+
+async function fetchData(): Promise<ApiResponse> {
+    try {
+        const response: AxiosResponse<ApiResponse> = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.log('Error');
+        throw error;
+    }
+}
+
+export { fetchData };
