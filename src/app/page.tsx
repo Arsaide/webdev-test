6e5c5165a19947d192f5e9cfd711b81c2f7f71cd
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-11 22:14:15 +0200

    Fixed deploy bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import FilterPanel from "@/components/pages/main-page/goods/filter-panel/FilterPanel";
 import Basket from "@/components/pages/main-page/basket/Basket";
-import SearchPanel from "@/components/pages/main-page/goods/search-panel/SearchPanel";
 
 export default function Home() {
   return (
     <>
       <div className='container'>
-          <SearchPanel/>
         <FilterPanel/>
         <Basket/>
       </div>
