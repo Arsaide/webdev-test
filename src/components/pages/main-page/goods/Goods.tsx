a91ffee797e9835ba62277eb17151b4e23b9410f
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-11 02:58:04 +0200

    Needs improvement Goods component

--- src/components/pages/main-page/goods/Goods.tsx
+++ src/components/pages/main-page/goods/Goods.tsx
@@ -4,23 +4,23 @@ import {GoodsCard} from "@/components/pages/main-page/goods/subcomponents/goods-
 import useData from "@/hooks/use-data/UseData";
 
 const Goods = () => {
-    const { products } = useData()
+    const { getFilteredProducts } = useData()
 
     return (
         <>
-            {products.map(item => (
-                <GoodsCard
-                    key={item.id}
-                    id={item.id}
-                    title={item.title}
-                    description={item.description}
-                    price={item.price}
-                    discountPercentage={item.discountPercentage}
-                    rating={item.rating}
-                    brand={item.brand}
-                    category={item.category}
-                    images={item.images}/>
-            ))}
+            {/*{getFilteredProducts().map(item => (*/}
+            {/*    <GoodsCard*/}
+            {/*        key={item.id}*/}
+            {/*        id={item.id}*/}
+            {/*        title={item.title}*/}
+            {/*        description={item.description}*/}
+            {/*        price={item.price}*/}
+            {/*        discountPercentage={item.discountPercentage}*/}
+            {/*        rating={item.rating}*/}
+            {/*        brand={item.brand}*/}
+            {/*        category={item.category}*/}
+            {/*        images={item.images}/>*/}
+            {/*))}*/}
         </>
     );
 };
