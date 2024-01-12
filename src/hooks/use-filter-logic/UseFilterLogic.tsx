457eb0db7ebc4970bbf63581cdfb1f9c195d09be
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-12 13:38:04 +0200

    Updated UseFilterLogic hook

--- src/hooks/use-filter-logic/UseFilterLogic.tsx
+++ src/hooks/use-filter-logic/UseFilterLogic.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import useData from '@/hooks/use-data/UseData';
+import usePagination from "@/hooks/use-pagination/UsePagination";
 
 const useFilterLogic = (
     searchQuery: string,
     selectedCategory: string,
-
     // Типизации функции стейта
     // Она принимает строку или функцию которая возвращающает строку
     // Помогает предотвратить ошибки типизации со стейтом
@@ -12,19 +12,21 @@ const useFilterLogic = (
     ) => {
 
     const { getFilteredProducts, getSearchResults } = useData();
+    const { currentPage, setCurrentPage } = usePagination();
 
     // Смена состояния категории
     const handleCategoryChange = (category: string) => {
         setSelectedCategory(category);
+        setCurrentPage(1); // Сбрасываем страницу при изменении категории
     };
 
-
-    // Сбрас категории при вводе в поиск
+    // Сбрас категории и страницы при вводе в поиск
     useEffect(() => {
         if (searchQuery !== '') {
             setSelectedCategory('');
+            setCurrentPage(1); // Сбрасываем страницу при вводе в поиск
         }
-    }, [searchQuery, setSelectedCategory]);
+    }, [searchQuery, setSelectedCategory, setCurrentPage]);
 
     // Проверка на пустую строку
     const filteredProducts =
@@ -35,6 +37,5 @@ const useFilterLogic = (
         filteredProducts,
     };
 };
-
 
 export default useFilterLogic;
