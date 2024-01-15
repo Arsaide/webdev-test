07a7bde5b28338cc864c36c0575a02c972b391a5
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-15 23:33:55 +0200

    attempt to implement a shopping cart

--- src/components/pages/main-page/basket/Basket.tsx
+++ src/components/pages/main-page/basket/Basket.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {Drawer, List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
+import {ShoppingBasket} from "@mui/icons-material";
 
-function Basket() {
+const Basket = (props: { cartOpen: boolean, closeCart: any, order: string; removeFromOrder: string; }) => {
+    const {
+        cartOpen,
+        closeCart = Function.prototype,
+        order,
+        removeFromOrder,
+    } = props
+
     return (
-        <section>
-        </section>
+        <Drawer
+            anchor={'right'}
+            open={cartOpen}
+            onClose={closeCart}>
+            <List>
+                <ListItem>
+                    <ListItemIcon>
+                        <ShoppingBasket/>
+                    </ListItemIcon>
+                    <ListItemText primary={"Cart"}/>
+                </ListItem>
+            </List>
+
+        </Drawer>
     );
 }
 
