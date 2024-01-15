1f8085ecfd55bf004dc169b0ae100871764f04cc
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-15 23:34:53 +0200

    remove Basket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Goods from "@/components/pages/main-page/goods/Goods";
-import Basket from "@/components/pages/main-page/basket/Basket";
 
 export default function Home() {
     return (
         <>
             <Goods/>
-            <Basket/>
         </>
     )
 }
