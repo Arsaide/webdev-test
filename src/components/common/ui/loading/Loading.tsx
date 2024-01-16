cd7ae1105d7d42c5ae6afb9c6fbe5d6cfebc9477
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-17 00:34:00 +0200

    Modified Loading.tsx and added styles

--- src/components/common/ui/loading/Loading.tsx
+++ src/components/common/ui/loading/Loading.tsx
@@ -1,9 +1,16 @@
+import { Container } from '@mui/material';
 import React from 'react';
+import Image from "next/image";
+import loadingGif from '../../../../../public/loading.gif'
+import styles from "./Loading.module.scss"
 
 const Loading = () => {
     return (
         <>
-            Loading...
+            <Container sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <div style={{margin: '15px'}}><em>Please wait or use pagination!</em></div>
+                <Image className={styles.img} src={loadingGif} alt={'Loading gif'} width={700} height={500}/>
+            </Container>
         </>
     );
 };
