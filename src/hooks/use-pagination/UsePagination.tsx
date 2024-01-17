3765994de4f048049a5fbfde877fa4a507b8f9cc
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-18 00:10:33 +0200

    Added comment

--- src/hooks/use-pagination/UsePagination.tsx
+++ src/hooks/use-pagination/UsePagination.tsx
@@ -2,26 +2,28 @@ import React, { useState } from 'react';
 import { Product } from "@/app/api/dataService";
 
 interface PaginationHook {
-    currentPage: number;
-    productsPerPage: number;
-    getCurrentProducts: (products: Product[]) => Product[];
-    handlePageChange: (e: React.ChangeEvent<unknown>, page: number) => void;
-    setProductsPerPage: React.Dispatch<React.SetStateAction<number>>;
-    setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+    currentPage: number // Текущая страница
+    productsPerPage: number // Колво продуктов
+    getCurrentProducts: (products: Product[]) => Product[] // Получения текущих продуктов
+    handlePageChange: (e: React.ChangeEvent<unknown>, page: number) => void // Обработчик пагинации
+    setProductsPerPage: React.Dispatch<React.SetStateAction<number>> // Изменение продуктов
+    setCurrentPage: React.Dispatch<React.SetStateAction<number>> // Изменение страници
 }
 
 const usePagination = (initProducts: number = 9): PaginationHook => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [productsPerPage, setProductsPerPage] = useState(initProducts);
+    // Хранение данніх страници и продуктов
+    const [currentPage, setCurrentPage] = useState(1)
+    const [productsPerPage, setProductsPerPage] = useState(initProducts)
 
     const handlePageChange = (e: React.ChangeEvent<unknown>, page: number) => {
         setCurrentPage(page);
     };
 
+    // Продуктов которіе будут рендерится
     const getCurrentProducts = (products: Product[]) => {
         const lastProduct = currentPage * productsPerPage,
-            firstProduct = lastProduct - productsPerPage;
-        return products.slice(firstProduct, lastProduct);
+            firstProduct = lastProduct - productsPerPage
+        return products.slice(firstProduct, lastProduct)
     };
 
     return {
